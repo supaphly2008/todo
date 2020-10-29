5829077b0b17ffd11bce5b2e2c95225e0e9de762
terry <liaoiyang@gmail.com>
2020-10-30 00:23:45 +0800

    add dialog when add todo button is clicked

---
+++ src/components/Modal.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-row justify="center">
+    <v-dialog :value="dialog" max-width="290" @click:outside="closeModal">
+      <v-card>
+        <v-card-title class="headline">
+          Use Google's location service?
+        </v-card-title>
+
+        <v-card-text>
+          Let Google help apps determine location. This means sending anonymous
+          location data to Google, even when no apps are running.
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn color="green darken-1" text @click="closeModal">
+            Disagree
+          </v-btn>
+
+          <v-btn color="green darken-1" text @click="closeModal">
+            Agree
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    dialog: Boolean
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    closeModal() {
+      this.$emit("closeModal");
+    }
+  }
+};
+</script>
