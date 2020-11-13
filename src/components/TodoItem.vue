69634d061511bfaae337ac8e4cd5ace3ca0d6934
Terry Liao <terry.liao@nogle.com>
2020-11-14 01:23:23 +0800

    add date-fns
    add todos created  time

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,53 +1,63 @@
 <template>
-  <v-list-item>
-    <v-list-item-content>
-      <v-list-item-title>{{ todo.title }}</v-list-item-title>
-    </v-list-item-content>
-    <v-list-item-icon>
-      <v-tooltip left>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            @click.stop="completeTodo(todo)"
-            icon
-            color="green"
-            dark
-            v-bind="attrs"
-            v-on="on"
-          >
-            <v-icon>fas fa-check-circle</v-icon>
-          </v-btn>
-        </template>
-        <span>Mark done</span>
-      </v-tooltip>
-    </v-list-item-icon>
-    <v-list-item-icon>
-      <v-tooltip right>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            @click.stop="deleteTodo(todo)"
-            icon
-            color="pink"
-            dark
-            v-bind="attrs"
-            v-on="on"
-          >
-            <v-icon>fas fa-trash-alt</v-icon>
-          </v-btn>
-        </template>
-        <span>Delete</span>
-      </v-tooltip>
-    </v-list-item-icon>
-  </v-list-item>
+  <div>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title>{{ todo.title }}</v-list-item-title>
+        <v-list-item-subtitle>{{ createdTime }}</v-list-item-subtitle>
+      </v-list-item-content>
+      <v-list-item-icon>
+        <v-tooltip left>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              @click.stop="completeTodo(todo)"
+              icon
+              color="green"
+              dark
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-icon>fas fa-check-circle</v-icon>
+            </v-btn>
+          </template>
+          <span>Mark done</span>
+        </v-tooltip>
+      </v-list-item-icon>
+      <v-list-item-icon>
+        <v-tooltip right>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              @click.stop="deleteTodo(todo)"
+              icon
+              color="pink"
+              dark
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-icon>fas fa-trash-alt</v-icon>
+            </v-btn>
+          </template>
+          <span>Delete</span>
+        </v-tooltip>
+      </v-list-item-icon>
+    </v-list-item>
+  </div>
 </template>
 
 <script>
+import { format } from "date-fns";
 export default {
   props: {
     todo: {
       title: String,
       id: String,
-      isDone: Boolean
-    }
+      isDone: Boolean,
+      createdTime: Date,
+    },
+  },
+  computed: {
+    createdTime() {
+      return format(new Date(this.todo.createdTime), "yyyy/MM/dd HH:mm");
+    },
   },
   methods: {
     completeTodo(todo) {
@@ -55,7 +65,7 @@ export default {
     },
     deleteTodo(todo) {
       console.log("deleteTodo", todo);
-    }
-  }
+    },
+  },
 };
 </script>
