a8fcaa443dad434f42246c055ca574159348230a
Terry Liao <terry.liao@nogle.com>
2020-11-14 11:11:45 +0800

    add uuid for each todo item as id

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
+import UUID from "vue-uuid";
 
 Vue.config.productionTip = false;
+
+Vue.use(UUID);
 
 new Vue({
   render: (h) => h(App),
