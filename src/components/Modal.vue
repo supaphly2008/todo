80d1934ec9df399e03d1a20bb92c7300de04c4b2
Terry Liao <terry.liao@nogle.com>
2020-11-15 01:16:18 +0800

    add date picker

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -7,13 +7,53 @@
         <v-form>
           <v-container>
             <v-row>
-              <v-col>
+              <v-col cols="12" sm="8">
                 <v-text-field
                   v-model="form.title"
                   :counter="20"
                   label="Title"
                   required
                 ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="4">
+                <v-menu
+                  ref="menu"
+                  v-model="menu"
+                  :close-on-content-click="false"
+                  :return-value.sync="form.date"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="290px"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="form.date"
+                      label="Due Date"
+                      prepend-icon="mdi-calendar"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker
+                    :allowed-dates="allowedDates"
+                    v-model="form.date"
+                    no-title
+                    scrollable
+                  >
+                    <v-spacer></v-spacer>
+                    <v-btn text color="primary" @click="menu = false">
+                      Cancel
+                    </v-btn>
+                    <v-btn
+                      text
+                      color="primary"
+                      @click="$refs.menu.save(form.date)"
+                    >
+                      OK
+                    </v-btn>
+                  </v-date-picker>
+                </v-menu>
               </v-col>
             </v-row>
           </v-container>
@@ -40,6 +80,7 @@
 
 <script>
 import { MODAL_TYPE } from "./Todo";
+import { formatDateTime } from "../utils";
 
 export default {
   props: {
@@ -51,7 +92,10 @@ export default {
     return {
       form: {
         title: "",
+        // date: new Date().toISOString().substr(0, 10),
+        date: formatDateTime(new Date(), "yyyy-MM-dd").substr(0, 10),
       },
+      menu: false,
     };
   },
   computed: {
@@ -63,6 +107,9 @@ export default {
     },
   },
   methods: {
+    allowedDates(val) {
+      return parseInt(val.split("-")[2], 10) % 2 === 0;
+    },
     closeModal() {
       this.form.title = "";
       this.$emit("closeModal");
